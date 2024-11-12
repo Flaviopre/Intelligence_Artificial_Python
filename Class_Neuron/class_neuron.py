052bc7a8905cb59d0dd20f270fad29021d1630d8
Flavio Fernandes De Araujo <fernflavio@icloud.com>
2024-11-12 12:12:08 +0100

    Modification de la base de la classe avec initialisation d'une liste contenant des nombres flottants de -1 à 1 en choissant un nombre qui sera la taille de la neurone

--- Class_Neuron/class_neuron.py
+++ Class_Neuron/class_neuron.py
@@ -1,3 +1,12 @@
-class class_neuron : 
-    pass
-
+# Importe le module 'random'
+import random
+# Crée une classe 'Neuron'
+class Neuron:
+    # Définit la méthode __init__ qui initialise les attributs de la classe 'Neuron'
+    def __init__(self, num_inputs):
+        # Initialise le nombre d'entrées du neurone
+        self.num_inputs = num_inputs
+        # Initialise une liste de coefficients avec des valeurs aléatoires
+        # La liste contient 'num_inputs + 1' éléments
+        # Chaque élément est un nombre aléatoire flottants entre -1 et 1
+        self.coefficients = [random.uniform(-1, 1) for i in range(num_inputs + 1)]
