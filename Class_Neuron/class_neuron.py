2c3f77f14182226fbb639793d302b439fe661e14
Flavio Fernandes De Araujo <fernflavio@icloud.com>
2024-11-13 11:26:57 +0100

    Correction variable num_inputs => num-imputs

--- Class_Neuron/class_neuron.py
+++ Class_Neuron/class_neuron.py
@@ -24,6 +24,6 @@ class Neuron:
     def getCoefficient(self, position):
         if not isinstance(position, int):
             raise ValueError("La position doit être un entier.")
-        elif position < 0 or position >= len(self.coefficients) or position == 0 :
-            raise ValueError("La position doit être comprise entre 1 et " + str(self.num_imputs) + ".")
+        elif position < 0 or position >= len(self.coefficients) or position == 0 or position == None :
+            raise ValueError("La position doit être comprise entre 1 et " + str(self.num_inputs) + ".")
         return self.coefficients[position]
