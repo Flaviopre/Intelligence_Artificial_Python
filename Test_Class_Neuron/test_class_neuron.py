f6e0809ed07a17a53305c69d1da93cbf58bd2568
Flavio Fernandes De Araujo <fernflavio@icloud.com>
2024-11-13 10:07:25 +0100

    Vérification du nombres de coefficients dans la liste d'un neurone

--- Test_Class_Neuron/test_class_neuron.py
+++ Test_Class_Neuron/test_class_neuron.py
@@ -17,7 +17,16 @@ class Vérification_Nombre_Entrées(unittest.TestCase):
         test1 = Neuron(3)
         # Vérifie que le nombre d'entrées du neurone est égal à 3
         self.assertEqual(test1.num_inputs, 3)
-            
+
+# Crée une classe 'Vérification_Liste_Coefficients' pour tester le nombre de coefficients dans la liste d'un neurone
+class Vérification_Liste_Coefficients(unittest.TestCase):
+    # Définit la méthode 'test_init' pour tester l'initialisation d'un neurone
+    def test_init(self):
+        # Crée un neurone avec 3 entrées
+        test1 = Neuron(3)
+        # Vérifie que la longueur de la liste de coefficients est égale à 4
+        self.assertEqual(len(test1.coefficients), 4)
+
 # Définit la méthode 'test_compute' pour tester le calcul d'un neurone        
 if __name__ == '__main__':
     unittest.main()   
