9776ccc70e8e70aa830cbaf5fad66e2b124dd5cd
Flavio Fernandes De Araujo <fernflavio@icloud.com>
2024-11-22 09:53:58 +0100

    Renommage du fichier/dossier Class_SigmoidNeuuron.py

---
+++ Class_SigmoidNeuron/Class_SigmoidNeuron.py
@@ -0,0 +1,22 @@
+# Importation de la librairie math
+import math
+
+# Définition de la fonction sigmoid
+def sigmoid(x):
+    # Vérifie que x est un entier
+    if not isinstance(x, int):
+        # Lève une erreur si x n'est pas un entier
+        raise ValueError("x doit être un entier")
+    # Si x est supérieur à 100
+    if x > 100:
+        # Retourne 1.0
+        return 1.0
+    # Sinon si x est inférieur à -100
+    elif x < -100:
+        # Retourne 0.0
+        return 0.0
+    # Sinon
+    else:
+        # Retourne 1 / (1 + exp(-x))
+        return 1 / (1 + math.exp(-x))
+    
