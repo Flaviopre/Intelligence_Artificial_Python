5d2d265b78e6dae5fb3de1abf2ee4ce82db86f6e
Flaviopre <fernflavio@icloud.com>
2025-01-22 18:13:28 +0100

    Réarengement de la fonction Test

--- Test_Classe_Learning/Test_Classe_Learning.py
+++ Test_Classe_Learning/Test_Classe_Learning.py
@@ -10,29 +10,26 @@ import unittest
 # Importer le fichier Classe_Learning du dossier Classe_Learning
 from Classe_Learning.Classe_Learning import Learning
 
-# Importation de numpy pour les tests
-import numpy as np
-
 # Véirifier le raiseError lors de la création de la classe Learning
 class TestLearning(unittest.TestCase): 
     
     # Test des entrées et des sorties si égale 
     def test_input_output_égale(self):
         # Création des inputs avec une liste de listes (matrice)
-        inputs = np.array([[0.1, 0.2, 0.3], [0.4, 0.5, 0.6], [0.7, 0.8, 0.9]])
+        inputs = [[0.1, 0.2, 0.3], [0.4, 0.5, 0.6], [0.7, 0.8, 0.9]]
         # Création des outputs avec une liste monodimensionnelle qui ets une liste basique
-        outputs = np.array([0.1, 0.2, 0.3])
+        outputs = [0.1, 0.2, 0.3]
         # Création de l'instance de la classe Learning
         learning_instance = Learning(0, inputs ,outputs)
         # Teste si la première dimension de inputs est égale à celle de outputs
-        self.assertEqual(learning_instance._Learning__inputs.shape[0], learning_instance._Learning__outputs.shape[0])
+        self.assertEqual(learning_instance._Learning__inputs[0], learning_instance._Learning__outputs)
     
     # Test des entrées et des sorties si différente et lève une erreur
     def test_input_output_différent(self):
         # Création des inputs avec une liste de listes (matrice)
-        inputs = np.array([[0.1, 0.2, 0.3], [0.4, 0.5, 0.6], [0.7, 0.8, 0.9]])
+        inputs = [[0.1, 0.2, 0.3], [0.4, 0.5, 0.6], [0.7, 0.8, 0.9]]
         # Création des outputs avec une liste monodimensionnelle qui ets une liste basique
-        outputs = np.array([0.1, 0.2])
+        outputs = [0.1, 0.2]
         # Regarde l'erreur levée
         with self.assertRaises(ValueError):
             # Création de l'instance de la classe Learning
