a68857462e306c8b76e0c277a7072ae34afaa38c
Flavio Fernandes De Araujo <fernflavio@icloud.com>
2025-01-22 14:21:58 +0100

    Importation des modules pour les importations

---
+++ Classe_Learning/Classe_Learning.py
@@ -0,0 +1,5 @@
+# Importation des modules nécessaires pour l'ajout du chemin parent au sys.path
+import sys
+import os
+# Ajouter le chemin parent au sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
