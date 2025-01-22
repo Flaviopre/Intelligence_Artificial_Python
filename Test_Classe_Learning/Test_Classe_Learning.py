12593b592c7555cdfc96d27a8d04db42df394d6f
Flavio Fernandes De Araujo <fernflavio@icloud.com>
2025-01-22 15:10:08 +0100

    Ajout de la vérification des entrées

---
+++ Test_Classe_Learning/Test_Classe_Learning.py
@@ -0,0 +1,31 @@
+# Importation des modules nécessaires pour l'ajout du chemin parent au sys.path
+import sys
+import os
+# Ajouter le chemin parent au sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# Importe le module 'unittest'
+import unittest
+
+# Importer le fichier Classe_Learning du dossier Classe_Learning
+from Classe_Learning.Classe_Learning import Learning
+
+# Importation de numpy pour les tests
+import numpy as np
+
+# Véirifier le raiseError lors de la création de la classe Learning
+class TestLearning(unittest.TestCase): 
+    
+    # Test des entrées et des sorties si égale 
+    def test_input_output_égale(self):
+        # Création des inputs avec une liste de listes (matrice)
+        inputs = np.array([[0.1, 0.2, 0.3], [0.4, 0.5, 0.6], [0.7, 0.8, 0.9]])
+        # Création des outputs avec une liste monodimensionnelle qui ets une liste basique
+        outputs = np.array([0.1, 0.2, 0.3])
+        # Création de l'instance de la classe Learning
+        learning_instance = Learning(0, inputs ,outputs)
+        # Teste si la première dimension de inputs est égale à celle de outputs
+        self.assertEqual(learning_instance._Learning__inputs.shape[0], learning_instance._Learning__outputs.shape[0])
+    
+if __name__ == '__main__':
+    unittest.main()     
