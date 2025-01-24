5669e570711f6d443c2bf912913fd19e7c7c01ee
Flavio Fernandes De Araujo <fernflavio@icloud.com>
2025-01-24 10:14:33 +0100

    Ajout des tests

--- Test_Class_NeuralNetwork/Test_Class_NeuralNetwork.py
+++ Test_Class_NeuralNetwork/Test_Class_NeuralNetwork.py
@@ -4,12 +4,6 @@ import os
 # Ajouter le chemin parent au sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-# Importation de la classe Neuron du dossier Classe_Neuron
-from Class_Neuron.class_neuron import Neuron
-
-# Importation du module class sigmoid_neuron
-from Class_SigmoidNeuron.Class_SigmoidNeuron import SigmoidNeuron
-
 # Importation de la class NeuralNetwork du dossier Class_NeuralNetwork
 from Class_NeuralNetwork.Class_NeuralNetwork import NeuralNetwork
 
@@ -69,31 +63,34 @@ class TestNeuralNetwork(unittest.TestCase):
                     self.assertEqual(neuronal_network._NeuralNetwork__layers[layer][neuron].getCoefficient(coeff), random_number)
     
     def test_get_outputs(self):
+    
         # Création d'un objet de la classe NeuralNetwork avec 2 couches de 2 neurones linéaires chacune
-        neuronal_network = NeuralNetwork(2, [2, 2], ["NEU", "NEU"])
+        neuronal_network = NeuralNetwork(3, [2, 3], ["SIG", "SIG"])
         
         # Initialisation de tous les coefficients à zéro
         for layer in range(len(neuronal_network._NeuralNetwork__layers)):
-            # Regarde chaque neurone de chaque couche du réseau de neurones
             for neuron in range(len(neuronal_network._NeuralNetwork__layers[layer])):
-                # Regarde chaque coefficient de chaque neurone de chaque couche du réseau de neurones
-                for coeff in range(neuronal_network._NeuralNetwork__layers[layer][neuron].getNeuronSize()):
-                    # Modification du coefficient du neurone à 0.0
+                for coeff in range(1, neuronal_network._NeuralNetwork__layers[layer][neuron].getNeuronSize() + 1):
                     neuronal_network._NeuralNetwork__layers[layer][neuron].setCoefficient(coeff, 0.0)
         
-        # Modification du premier coefficient du deuxième neurone dans la première couche à 1.0
-        neuronal_network._NeuralNetwork__layers[0][1].setCoefficient(0, 1.0)
-        
-        # Modification du deuxième coefficient du deuxième neurone dans la deuxième couche à 1.0
-        neuronal_network._NeuralNetwork__layers[1][1].setCoefficient(1, 1.0)
+        # Modification des coefficients spécifiques
+        neuronal_network._NeuralNetwork__layers[0][1].setCoefficient(3, 1.0)
+        neuronal_network._NeuralNetwork__layers[1][2].setCoefficient(2, 1.0)
         
         # Test des sorties pour une entrée spécifique
-        inputs = [1.0, 2.0]
+        inputs = [1.0, 0.0, 0.0]
+        # Affichage des coefficients des neurones pour vérification
+        for layer in range(len(neuronal_network._NeuralNetwork__layers)):
+            for neuron in range(len(neuronal_network._NeuralNetwork__layers[layer])):
+                coefficients = [neuronal_network._NeuralNetwork__layers[layer][neuron].getCoefficient(i + 1) for i in range(neuronal_network._NeuralNetwork__layers[layer][neuron].getNeuronSize())]
+                print(f"Layer {layer}, Neuron {neuron}, Coefficients: {coefficients}")
+        
         outputs = neuronal_network.getOutputs(inputs)
-
+        
         # La valeur de sortie doit correspondre à l'entrée concernée
-        self.assertEqual(outputs, [1.0, 2.0])
-
+        self.assertEqual(outputs, [0.0, 0.0, 1.0])
+    
+    
 if __name__ == '__main__':
     unittest.main()
     
